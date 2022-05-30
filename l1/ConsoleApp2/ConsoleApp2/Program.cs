2c257452306f6d5f8cab088dd78ac6bd78275233
Xenoneko <84183255+Catgirllover@users.noreply.github.com>
2022-05-30 08:08:27 +0300

    Add files via upload

---
+++ l1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.WriteLine("!!!");
+            Console.ReadLine();
+        }
+    }
+}
