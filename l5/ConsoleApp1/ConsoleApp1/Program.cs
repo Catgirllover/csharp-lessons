ee244703a13afdfa6c3eb26a5b4b7b0bcebda549
Xenoneko <84183255+Catgirllover@users.noreply.github.com>
2022-06-02 12:24:04 +0300

    Add files via upload

---
+++ l5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static string[] Splitter(string phrase)
+        {
+            string[] phraseArr = phrase.Split(' ');
+            return phraseArr;
+        }
+        public static void Printer(string[] toPrint)
+        {
+            foreach (var printed in toPrint)
+            {
+                Console.WriteLine(printed);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] phraseArr = Splitter(text);
+            Printer(phraseArr);
+        }
+    }
+}
---
+++ l5/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static string[] Splitter(string phrase)
+        {
+            string[] phraseArr = phrase.Split(' ');
+            return phraseArr;
+        }
+
+
+        public static void ReverseWords(string inputPhrase)
+        {
+            string dataHolder;
+            string[] phraseArr = Splitter(inputPhrase);
+            for (int i = 0; i < phraseArr.Length; i++)
+            {
+                if (phraseArr.Length % 2 == 0)
+                {
+                    if (i < phraseArr.Length / 2)
+                    {
+                        dataHolder = phraseArr[phraseArr.Length-1 - i];
+                        phraseArr[phraseArr.Length-1 - i] = phraseArr[i];
+                        phraseArr[i] = dataHolder;
+                    }
+                }
+                else if (i < phraseArr.Length / 2)
+                {
+                    dataHolder = phraseArr[phraseArr.Length-1 - i];
+                    phraseArr[phraseArr.Length-1 - i] = phraseArr[i];
+                    phraseArr[i] = dataHolder;
+                }
+            }
+            foreach (var i in phraseArr)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            ReverseWords(text);
+        }
+    }
+}
